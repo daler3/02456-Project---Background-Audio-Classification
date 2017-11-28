bcf2b943fd81265301f34529a7489b5a9e62db30
Luca Cambiaghi <lc.cambiaghi@gmail.com>
2017-11-28 08:21:54 +0100

    Now it aumoatically adjusts the learning rate according to segment length

--- keras_models.py
+++ keras_models.py
@@ -3,10 +3,11 @@ from keras.layers.core import Dense, Activation, Dropout, Flatten
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras import regularizers
+from keras.optimizers import sgd
 
 
 def piczak_CNN(input_dim, output_dim,
-        activation='relu', optimizer="adam",
+        activation='relu',
         metrics="accuracy", loss='categorical_crossentropy'):
     """
     This method returns a keras model describing Piczak implementation.
@@ -19,10 +20,10 @@ def piczak_CNN(input_dim, output_dim,
     """
 
     model = Sequential()
-    model.add(Conv2D(80, kernel_size=(input_dim[0]-3, 6), strides=(1, 1),
+    model.add(Conv2D(80, kernel_size=(input_dim[0] - 3, 6), strides=(1, 1),
                      activation=activation,
-                     input_shape=input_dim,
-                     kernel_regularizer=regularizers.l2(0.001)))
+                     input_shape=input_dim))
+                     #kernel_regularizer=regularizers.l2(0.001)))
 
     model.add(MaxPooling2D(pool_size=(4, 3), strides=(1, 3)))
     model.add(Dropout(0.5))
@@ -49,10 +50,16 @@ def piczak_CNN(input_dim, output_dim,
     model.add(Dense(output_dim))
     model.add(Activation('softmax'))
 
+    if input_dim[1] == 41:
+        optimizer = sgd(lr=0.002, momentum=0.9, nesterov=True, decay=0.001)
+    else:
+        if input_dim[1] == 101:
+            optimizer = sgd(lr=0.01, momentum=0.9, nesterov=True, decay=0.001)
+        else:
+            raise ValueError("Segment must either have 41 or 101 frames")
+
     model.compile(loss=loss,
                   optimizer=optimizer,
                   metrics=[metrics])
-
-    print("Model built")
 
     return model
