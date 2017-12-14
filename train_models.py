050411d5b5546962ff0797c8f5338e262f34ea4b
Luca Cambiaghi <lc.cambiaghi@gmail.com>
2017-12-14 18:17:19 +0100

    Version used for cross-validation

--- train_models.py
+++ train_models.py
@@ -2,24 +2,37 @@ from preprocessor import preprocessor
 from keras_models import piczak_CNN
 from keras.callbacks import TensorBoard, EarlyStopping
 from sklearn import metrics, model_selection
+from keras.models import load_model
 import numpy as np
 import pandas as pd
 import logging
 from keras import optimizers
-
+import utils
 
 def piczac_cross_validation(epochs, load_path):
+    train_dirs = []
+
+    logging.basicConfig(filename='cv.log', filemode='w', level=logging.DEBUG)
+
+    n_folders = 10
+    for i in range(1, n_folders + 1):
+        train_dirs.append('fold{0}'.format(i))
+
     cvscores = []
+    # (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10)
     for folds in ((10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10)):
         val_fold = 'fold' + str(folds[0])
         test_fold= 'fold' + str(folds[1])
+        #val_fold = 'fold6'
+        #test_fold = 'fold7'
         train_dirs.remove(val_fold)
         train_dirs.remove(test_fold)
 
         print("Run {0}: test folder is fold{0}".format(folds[1]) + ", validation folder is fold{0}".format(folds[0]))
+        logging.info("Run {0}: test folder is fold{0}".format(folds[1]) + ", validation folder is fold{0}".format(folds[0]))
 
-        tb = TensorBoard(log_dir='./TensorBoard/' + 'run{0}'.format(folds[1]))
-        es = EarlyStopping(patience=10, verbose=1)
+        tb = TensorBoard(log_dir='./TensorBoard/short_60/' + 'run{0}'.format(folds[1]))
+        es = EarlyStopping(patience=7, verbose=1)
 
         pp = preprocessor()
         pp.load_extracted_fts_lbs(load_path=load_path, train_dirs=train_dirs, test_fold=test_fold, val_fold=val_fold)
@@ -31,16 +44,18 @@ def piczac_cross_validation(epochs, load_path):
         print("Model built")
 
         model.fit(pp.train_x, pp.train_y, validation_data=[pp.val_x, pp.val_y], epochs=epochs,
-                  batch_size=1000, verbose=2, callbacks=[tb, es])
+                   batch_size=1000, verbose=2, callbacks=[tb, es])
         print("Model trained")
 
-        #output_model_file = 'models/long_' + str(epochs) + '_' + str(folds) + '.h5'
-        #logging.info("Saving the model to file: {0}".format(output_model_file))
-        #model.save(output_model_file)
+        output_model_file = 'models/long200_' + str(epochs) + '_' + str(folds) + '.h5'
+        #output_model_file = 'models/long200_150_(6, 7).h5'
+        # logging.info("Saving the model to file: {0}".format(output_model_file))
+        model.save(output_model_file)
+        #model = load_model(output_model_file)
 
         scores = model.evaluate(pp.test_x, pp.test_y, verbose=0)
         print("loss: {0}, test-acc: {1}".format(scores[0], scores[1]))
-        #logging.info("loss: {0}, test-acc: {1}".format(scores[0], scores[1]))
+        logging.info("loss: {0}, test-acc: {1}".format(scores[0], scores[1]))
 
         #logging.info("Writing test predictions to csv file : {0}".format(output_predictions_file))
         #def write_preds(preds, fname):
@@ -54,7 +69,33 @@ def piczac_cross_validation(epochs, load_path):
     print("Average performance after cross-validation: %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
 
 
+def plot_confusion_matrix(model_filename, load_path, save=False):
+    classes = ['air_conditioner', 'car_horn', 'children_playing', 'dog_bark', 'drilling', 'engine_idling', 'gun_shot',
+               'jackhammer', 'siren', 'street_music']
+
+    model = load_model(model_filename)
+    # example: long200_150_(1,2).h5
+    test_fold = 'fold' + model_filename.split(', ')[1].split(')')[0]
+
+    pp = preprocessor()
+    pp.load_extracted_fts_lbs(train_dirs=[test_fold], load_path=load_path)
+    preds = model.predict_classes(pp.train_x, verbose=0)
+    # write_preds(preds, output_predictions_file)
+    cm = metrics.confusion_matrix(np.argmax(pp.train_y, axis=1), preds)
+    if save:
+        utils.save_confusion_matrix(cm, classes)
+    else:
+        utils.plot_confusion_matrix(cm, classes)
+
 def scikit_cross_validation(epochs, load_path):
+    train_dirs = []
+
+    logging.basicConfig(filename='cv.log', filemode='w', level=logging.DEBUG)
+
+    n_folders = 10
+    for i in range(1, n_folders + 1):
+        train_dirs.append('fold{0}'.format(i))
+
     pp = preprocessor()
     pp.load_extracted_fts_lbs(train_dirs=train_dirs,
                  load_path=load_path)  # test_fold=test_dir,
@@ -85,16 +126,10 @@ def scikit_cross_validation(epochs, load_path):
 
 
 if __name__ == '__main__':
-    classes = ['air_conditioner', 'car_horn', 'children_playing', 'dog_bark', 'drilling', 'engine_idling', 'gun_shot',
-               'jackhammer', 'siren', 'street_music']
-    train_dirs = []
-
-    #logging.basicConfig(filename='cv.log', filemode='w', level=logging.DEBUG)
-
-    n_folders = 10
-    for i in range(1, n_folders + 1):
-        train_dirs.append('fold{0}'.format(i))
-
     # if using long segments, use 150 epochs. if using short, use 300
-    # piczac_cross_validation(epochs=150, load_path='extracted_short_200')
-    scikit_cross_validation(epochs=150, load_path='extracted_short_200')
+    # change tensorboard folder and model output file
+    piczac_cross_validation(epochs=300, load_path='extracted_short_60')
+    #scikit_cross_validation(epochs=150, load_path='extracted_long_60')
+    #model_filename = 'models/long60/long60_150_(9, 10).h5'
+    #load_path = 'data/extracted_long_60'
+    #plot_confusion_matrix(model_filename, load_path)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 import numpy as np
 
 def plot_confusion_matrix(cm, classes,
@@ -36,3 +37,40 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+
+
+def save_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.get_cmap(name='Blues')):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    import itertools
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation='vertical')
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 1.5
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig("confusion_matrix/cm.png", dpi=300)
